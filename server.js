83298e51f46da0be86c1861a7d0b570f4fea523c
anooja62 <anooja7777@gmail.com>
2023-05-04 21:32:51 +0530

    added

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const express = require("express");
 const mongoose = require("mongoose");
 const Cors = require("cors");
@@ -14,10 +16,10 @@ const messageRoutes = require("./routes/Message");
 const cartRoutes = require("./routes/Cart");
 const paymentRoutes = require("./routes/Payment");
 const orderRoutes = require("./routes/Orders");
-const checklistRoutes = require("./routes/Checklist")
+const checklistRoutes = require("./routes/Checklist");
 const salaryRoutes = require("./routes/Salary");
-const feedbackRoutes = require("./routes/FeedBack")
-const inspectRoutes = require("./routes/Inspection")
+const feedbackRoutes = require("./routes/FeedBack");
+const inspectRoutes = require("./routes/Inspection");
 //app config
 const app = express();
 const port = 9000;
@@ -29,14 +31,11 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://deliorder.onrender.com",
-      `https://us1.locationiq.com/v1/search.php?key=pk.de89a66c75d2c7e2838b70033a082722&q=${address}&format=json`
-     
+      `https://us1.locationiq.com/v1/search.php?key=pk.de89a66c75d2c7e2838b70033a082722&q=${address}&format=json`,
     ],
     credentials: true,
-    
   })
 );
-
 
 const MONGO_URI =
   "mongodb+srv://admin:Uous7v8k5FVxNu9x@cluster0.fyfxtkt.mongodb.net/?retryWrites=true&w=majority";
@@ -48,10 +47,9 @@ mongoose
   .then(() => console.log("connected  to mongodb !!"))
 
   .catch((err) => console.log(err.message));
-  app.get("/", async (req, res) => {
-   
-    res.send("API is running");
-  });
+app.get("/", async (req, res) => {
+  res.send("API is running");
+});
 app.use("/auth", authRoutes);
 app.use("/rest", restaurantRoutes);
 app.use("/deli", deliveryRoutes);
@@ -63,10 +61,10 @@ app.use("/msg", messageRoutes);
 app.use("/cart", cartRoutes);
 app.use("/pay", paymentRoutes);
 app.use("/order", orderRoutes);
-app.use("/check",checklistRoutes);
-app.use("/salary",salaryRoutes);
-app.use("/feed",feedbackRoutes);
-app.use("/insp",inspectRoutes);
+app.use("/check", checklistRoutes);
+app.use("/salary", salaryRoutes);
+app.use("/feed", feedbackRoutes);
+app.use("/insp", inspectRoutes);
 app.listen(port, () => {
   console.log(`listening in : ${port}`);
 });
